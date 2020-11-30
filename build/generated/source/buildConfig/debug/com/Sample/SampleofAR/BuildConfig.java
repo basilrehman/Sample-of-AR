b56b670e68374e548a95ccdad68d878c27906b9b
basil imran <basil.rehman98@gmail.com>
2020-11-30 13:40:36 +0500

    Sample-Project-of-AR

---
+++ build/generated/source/buildConfig/debug/com/Sample/SampleofAR/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Sample.SampleofAR;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Sample.SampleofAR";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "8.3";
+}
